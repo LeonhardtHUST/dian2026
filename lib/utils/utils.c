ebc9f7b113f78bc680e2bfa0a8fb0ebec9ceacb2
Leonhardt <92073072+LeonhardtHUST@users.noreply.github.com>
2026-03-30 14:01:47 +0800

    feat: impl. max_list within lib utils

---
+++ lib/utils/utils.c
@@ -0,0 +1,14 @@
+int max_list(const uint8_t num, const int n, ...) {
+    if (num == 0) return 0;
+    int maximum = n;
+    va_list args;
+    va_start(args, n);
+    for (uint8_t i = 1; i < num; i++) {
+        int val = va_arg(args, int);
+        if (val > maximum) {
+            maximum = val;
+        }
+    }
+    va_end(args);
+    return maximum;
+}
--- lib/utils/utils.h
+++ lib/utils/utils.h
@@ -7,6 +7,7 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "nvs_flash.h"
+#include <stdarg.h>
 
 #define WS2812_PIN  48
 #define LIGHT_NUM   1
@@ -14,5 +15,7 @@
 // 使用 pdMS_TO_TICKS 保证在不足1个tick时至少延时1个tick，避免死循环
 #define ticks_from_ms(ms) (pdMS_TO_TICKS(ms) > 0 ? pdMS_TO_TICKS(ms) : 1)
 #define delay_ms(ms) vTaskDelay(ticks_from_ms(ms))
+
+extern int max_list(const uint8_t num, const int n, ...);
 
 #endif // UTILS_H
